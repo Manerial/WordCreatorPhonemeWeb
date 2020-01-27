d6e528be4b018b3f2f80290d7cda0be2dbad3e2e
Manerial <avr1l.dunmare@gmail.com>
2020-01-27 16:21:55 +0100

    improve the graphic interface

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
-    $("button").click(function(){
+    $("#createList").click(function(){
         var wordNumber = $("#wordNumber").val();
-        $("#wordList").empty();
+        $('#wordList').empty();
+        $('#wordList2').empty();
         for(var number = 0; number < wordNumber; number++) {
-            var newWord = createWord("");
-            var newWordPhonemes = replacePhonemes(newWord);
-            $("#wordList").append("<p>" + newWord + "\t" + newWordPhonemes + "</p></br>");
+            var newWord = "";
+            do {
+                newWord = createWord("");
+            } while (newWord.length > 20 || newWord.length < 2);
+            var newWordNoPhonemes = replacePhonemes(newWord);
+            var li1Element = "<li class=\"list-group-item list-group-item-action\" data-toggle=\"tooltip\" data-placement=\"left\" title=\"Click to copy\">";
+            var li2Element = "</li>";
+            $("#wordList").append(li1Element + newWord + li2Element);
+            $("#wordList2").append(li1Element + newWordNoPhonemes + li2Element);
         }
+        $("li").click(function(event) {
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val($(this).text()).select();
+            document.execCommand("copy");
+            $temp.remove();
+        });
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
     });
 });
